e22fdbe6dfb459c4dd244f9453459d20a177d12c
ml <30815731+slimlime@users.noreply.github.com>
2021-07-11 11:10:05 +1000

    feat(flowcontrol): add line checker to compare solver

--- flowcontrol/8-loops-functions-sqrt-newton-method-scaling.go
+++ flowcontrol/8-loops-functions-sqrt-newton-method-scaling.go
@@ -1,5 +1,3 @@
-// Exercise: Loops and Functions
-
 package main
 
 import (
@@ -14,22 +12,24 @@ func Sqrt(
 ) {
 	// Strange bouncing around from arbitrary guess rather than backwards?
 	z = 1.0
-	adjustmentError:= 1.0
-	
-	for ; math.Abs(adjustmentError) > 0.0000000000001;  {
+	adjustmentError := 1.0
+
+	for i := 0; math.Abs(adjustmentError) > 0.000000000000001; i++ {
 		guess := z * z
 		adjustmentError = guess - x
 		scaledDenominator := 2 * z
 		z -= adjustmentError / scaledDenominator
 
-		fmt.Printf("z %.2f, x %.2f, guess-squared %.2f, adjust %.2f, scaledDenominator %.2f\n",
-			z,
-			x,
-			guess,
-			adjustmentError,
-			scaledDenominator,
-		)
-		// "x", x, "guess", guess, adjustmentError, scaledDenominator, z)
+		if i%3 == 0 {
+
+			fmt.Printf("z %.2f, x %.2f, guess-squared %.2f, adjust %.2f, scaledDenominator %.2f\n",
+				z,
+				x,
+				guess,
+				adjustmentError,
+				scaledDenominator,
+			)
+		}
 	}
 
 	return
@@ -40,8 +40,8 @@ func Sqrt(
 
 //
 func main() {
-	fmt.Println("custom Sqrt", Sqrt(16))
-	fmt.Println("math   Sqrt", math.Sqrt(16))
+	fmt.Println("custom Sqrt", Sqrt(3))
+	fmt.Println("math   Sqrt", math.Sqrt(3))
 
 	// 1.4166666666666667
 	// 1.41421356237
@@ -52,12 +52,10 @@ func main() {
 	// custom 1.414213562373095
 	// math   1.4142135623730951
 
-
-
 	testArr := [...]float64{1, 2, 3}
 	testeArr := []float64{1, 2, 3}
 	someMapIterable := map[int]float64{1: 1.1}
-	
+
 	for i := range testArr {
 		fmt.Println(i)
 	}
@@ -67,6 +65,5 @@ func main() {
 	for k, v := range someMapIterable {
 		fmt.Println(k, v)
 	}
-
 }
 
